1cd0fce393fa220bccb70a9c3447b837762b0ef0
JosephRer <jrechdiner2002@gmail.com>
2025-06-19 16:05:06 +0300

    factorial added

--- math_function.py
+++ math_function.py
@@ -10,9 +10,15 @@ def mult(a, b):
 def div(a, b):
     return a / b
 
+def factorial(a):
+    if a == 0:
+        return 1
+    return a * factorial(a - 1)
+
 def welcome():
     print("welcome to the algorithm library")
 
 if __name__ == "__main__":
     welcome()
+    factorial(4)
     
