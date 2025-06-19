d85a55423133c205faa35e022bbbc2e1b724258c
david <davidrechdiner@gmail.com>
2025-06-19 16:23:28 +0300

    prime

--- math_function.py
+++ math_function.py
@@ -15,10 +15,22 @@ def factorial(a):
         return 1
     return a * factorial(a - 1)
 
+def is_prime(a, i = 2, count = 1):
+    if a <= 1:
+        return False
+    if a == 2:
+        return True
+    if a == i + 1:
+        return count == 2
+    if a % i == 0:
+        return is_prime(a, i + 1, count + 1)
+    return is_prime(a, i + 1, count)
+
 def welcome():
     print("welcome to the algorithm library")
 
 if __name__ == "__main__":
     welcome()
     print(factorial(4))
+    print(is_prime(7))
     
