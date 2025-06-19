379e8c8b2395597b8fe2cc21d319864f7ddf64bb
david <davidrechdiner@gmail.com>
2025-06-19 14:28:28 +0300

    הוסף קובץ ראשי עםפונקציית ברכה

---
+++ math_function.py
@@ -0,0 +1,18 @@
+def plus(a, b):
+    return a + b
+
+def minus(a, b):
+    return a - b
+
+def mult(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+def welcome():
+    print("welcome to the algorithm library")
+
+if __name__ == "__main__":
+    welcome()
+    
